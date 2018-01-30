0989faf7bf9df7ae0389c17403036f9955ad1bd8
maklus <gitaristas1998>
2018-01-30 16:10:12 +0200

    Pilnai pabaigtas 10 uždavinys

--- Katino_Vakarienė/Program.cs
+++ Katino_Vakarienė/Program.cs
@@ -10,43 +10,56 @@ namespace Katino_Vakarienė
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Kiek iš viso žuvų pagavo katinas?");
-            int VisoZuvu = Convert.ToInt32(Console.ReadLine());
+
             int parnestos = 0;
             double BendrasSvoris = 0;
             int KiekDideliu = 0;
             int KiekSkaniu = 0;
+            char testi ;
+            Console.WriteLine("Ar katinas pagavo bent vieną žuvį t=taip, n=ne ");
+            var ArPagavo = Convert.ToChar(Console.ReadLine());
 
-            while (VisoZuvu != 0)
+            if (ArPagavo=='t')
             {
-                Console.WriteLine("Ar žuvis buvo skani 1=taip, 0=Ne");
-                int Atsakymas = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine();
-
-                Console.WriteLine("Koks žuvelės svoris? ");
-                Double Svoris = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine();
-
-                if (Svoris > 1 && Atsakymas == 1)
+                do
                 {
-                    parnestos++;
-                    BendrasSvoris += Svoris;
-                }
+                    Console.WriteLine("Ar žuvis buvo skani 1=taip, 0=Ne");
+                    int Atsakymas = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
 
-                if (Svoris > 1)
-                {
-                    KiekDideliu++;
-                }
+                    Console.WriteLine("Koks žuvelės svoris? ");
+                    Double Svoris = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine();
 
-                if (Atsakymas == 1)
-                {
-                    KiekSkaniu++;
-                }
+                    if (Svoris > 1 && Atsakymas == 1)
+                    {
+                        parnestos++;
+                        BendrasSvoris += Svoris;
+                    }
 
-                VisoZuvu--;
+                    if (Svoris > 1)
+                    {
+                        KiekDideliu++;
+                    }
+
+                    if (Atsakymas == 1)
+                    {
+                        KiekSkaniu++;
+                    }
+
+                    Console.WriteLine("Ar dar žuvų yra? t-taip, n-ne.");
+                    testi = Convert.ToChar(Console.ReadLine());
+
+                } while (testi == 't');
+
+                Console.WriteLine("Atsakymas:{0} žuvelės, {1} kg. ", parnestos, BendrasSvoris);
+                Console.WriteLine("Didelių žuvelių - {0}, skanių žuvelių - {1} kg. ", KiekDideliu, KiekSkaniu);
             }
 
-            Console.WriteLine("Atsakymas:{0} žuvelės, {1} kg. ", parnestos, BendrasSvoris);
+            else
+            {
+                Console.WriteLine("Katinas nieko nepagavo");
+            }
         }
     }
 }
