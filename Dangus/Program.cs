084733fcc588cc186278d3e3edf5019b301bb3ea
maklus <gitaristas1998>
2018-01-30 14:13:50 +0200

    Darbas

---
+++ Dangus/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dangus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Įveskite ar dangus giedras jei giedras 1, kitu atveju 0 ?");
+            int ats = Convert.ToInt32(Console.ReadLine());
+
+            if (ats==1)
+            {
+                Console.WriteLine("Dangus yra giedras");
+            }
+
+            if(ats==0)
+            {
+                Console.WriteLine("Dangus nėra giedras");
+            }
+            else if(ats !=1 && ats !=0)
+            {
+                Console.WriteLine("Įvedėte blogą skaičių");
+            }
+
+        }
+
+
+    }
+}
