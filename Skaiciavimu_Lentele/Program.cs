5fe557fc242ae8b3cd3576013d2d861f67a1e508
maklus <gitaristas1998>
2018-01-30 14:56:49 +0200

    Darbas

---
+++ Skaiciavimu_Lentele/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skaiciavimu_Lentele
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x      |x^2    |x^3|    1/x| ");
+            for (int i = 1; i < 10; i++)
+            {
+                Console.Write(i+"\t");
+                Console.Write(i*i + "\t");
+                Console.Write(i*i*i + "\t");
+                Console.Write(Math.Round((double)1 / i, 2) + "\t");
+                Console.WriteLine();
+            }
+        }
+    }
+}
