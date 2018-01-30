ea3f699fc9da683443b9038590864755dc1647b1
maklus <gitaristas1998>
2018-01-30 15:27:22 +0200

    Darbas

---
+++ Katino_Vakarienė/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katino_Vakarienė
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kiek iš viso žuvų pagavo katinas?");
+            int VisoZuvu = Convert.ToInt32(Console.ReadLine());
+            int parnestos = 0;
+            double BendrasSvoris = 0;
+
+            while (VisoZuvu!=0)
+            {
+                Console.WriteLine("Ar žuvis buvo skani 1=taip, 0=Ne");
+                int Atsakymas = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                Console.WriteLine("Koks žuvelės svoris? ");
+                Double Svoris = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine();
+
+                if (Svoris>1 && Atsakymas==1)
+                {
+                    parnestos++;
+                    BendrasSvoris += Svoris;
+                }
+
+                VisoZuvu--;        
+            }
+
+            Console.WriteLine("Atsakymas:{0} žuvelės, {1} kg. ", parnestos, BendrasSvoris);
+        }
+    }
+}
---
+++ Skaiciu_Lentele_iki16/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skaiciu_Lentele_iki16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//?????
+        }
+    }
+}
